32495b4847a79368b64cb67cba4944b9e320543c
Zakarik <inconnu_jedi@hotmail.com>
2026-02-06 15:44:29 +0100

    Mise à jour pour la prochaine version du système

--- scripts/knight-compendium.js
+++ scripts/knight-compendium.js
@@ -20,6 +20,7 @@ Hooks.on('ready', () => {
   processPackVisibility('include-capacite2038', 'knight-compendium.armours-2038');
   processPackVisibility('include-capacitecodex', 'knight-compendium.armours-codex');
   processPackVisibility('include-capaciteatlas', 'knight-compendium.armours-atlas');
+  processAdlVisibility();
 });
 
 Hooks.on('knightSettingsChange', setting => {
@@ -33,6 +34,9 @@ Hooks.on('knightSettingsChange', setting => {
     case 'include-capaciteatlas':
       processPackVisibility(setting, 'knight-compendium.armours-atlas');
       break;
+    case 'adl':
+      processAdlVisibility();
+    break;
   }
 });
 
@@ -51,4 +55,49 @@ function processPackVisibility(setting, packName) {
       TRUSTED: ownershipLevel,
     },
   });
+}
+
+async function processAdlVisibility() {
+  if (!game.user.isGM) {
+    return;
+  }
+
+  const setting = game.settings.get('knight', 'adl');
+  const std = setting;
+  const adl = !setting;
+  const ownershipStdLevel = std ? 'LIMITED' : 'OBSERVER';
+  const ownershipAdlLevel = adl ? 'LIMITED' : 'OBSERVER';
+
+  const listStd = [
+    'knight-compendium.overdrives',
+    'knight-compendium.weapons-standard',
+    'knight-compendium.weapons-avance',
+    'knight-compendium.weapons-rare',
+    'knight-compendium.modules-standard',
+    'knight-compendium.modules-avance',
+    'knight-compendium.modules-rare'];
+  const listAdl = [
+    'knight-compendium.adl-overdrives',
+    'knight-compendium.adl-armes-1-standard',
+    'knight-compendium.adl-modules-1-standard'];
+
+  for(let p of listStd) {
+    await game.packs.get(p).configure({
+      private: std,
+      ownership: {
+        PLAYER: ownershipStdLevel,
+        TRUSTED: ownershipStdLevel,
+      },
+    });
+  }
+
+  for(let p of listAdl) {
+    await game.packs.get(p).configure({
+      private: adl,
+      ownership: {
+        PLAYER: ownershipAdlLevel,
+        TRUSTED: ownershipAdlLevel,
+      },
+    });
+  }
 }
